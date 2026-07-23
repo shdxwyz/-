3c12ba68f4460c3d9f0917a9f690a8a18bcbcaf4
shdxwyz <3454197049@qq.com>
2026-07-23 18:39:51 +0800

    稳定18秒完成预赛

--- code/turn_control.h
+++ code/turn_control.h
@@ -10,7 +10,7 @@
 #define TURN_CONTROL_TARGET_ANGLE_DEG          (90.0f)
 
 // 从转向起始 yaw 算起，相对 yaw 达到 85 度时结束转向。
-#define TURN_CONTROL_STOP_YAW_ANGLE_DEG        (80.0f)
+#define TURN_CONTROL_STOP_YAW_ANGLE_DEG        (90.0f)
 
 // yaw 异常或车辆堵转时的转向超时保护（ms）。
 #define TURN_CONTROL_TIMEOUT_MS                (500u)
@@ -18,9 +18,9 @@
 // ==================== 角度环 PID 参数 ====================
 
 // PID 输出为左右轮速度差，单位 m/s。
-#define TURN_CONTROL_ANGLE_PID_MAX_OUT         (3.5f)
-#define TURN_CONTROL_ANGLE_PID_MAX_IOUT        (1.0f)
-#define TURN_CONTROL_ANGLE_KP                  (0.012f)
+#define TURN_CONTROL_ANGLE_PID_MAX_OUT         (6.5f)
+#define TURN_CONTROL_ANGLE_PID_MAX_IOUT        (2.0f)
+#define TURN_CONTROL_ANGLE_KP                  (0.016f)
 #define TURN_CONTROL_ANGLE_KI                  (0.00f)
 #define TURN_CONTROL_ANGLE_KD                  (0.00f)
 
--- code/yqj.c
+++ code/yqj.c
@@ -89,7 +89,8 @@ uint8 yqj_erjiguan_trigger(const uint16 adc_value[])
 
 // 特别二极管：A14检测到白线
 uint8 yqj_tberjiguan_trigger(const uint16 adc_value[])
-{   return (adc_value[14] < YQJ_TURN_TRIGGER_ADC_VALUE);}
+{   return (adc_value[14] < YQJ_TURN_TRIGGER_ADC_VALUE||
+        adc_value[13] < YQJ_TURN_TRIGGER_ADC_VALUE);}
 
 // 三极管 1_2：A1 和 A2 同时检测到白线
 uint8 yqj_sanjiguan1_2trigger(const uint16 adc_value[])
